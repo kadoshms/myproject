2f35181b1437cfd124b2a60e130b7c7c7873aaa4
mor <kadoshms@gmail.com>
2014-06-29 20:16:26 +0300

    fixed form and mailing

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -59,12 +59,12 @@ class PageController extends Controller
                 $message = \Swift_Message::newInstance()
                     ->setSubject('Contact enquiry from symblog')
                     ->setFrom('enquiries@symblog.co.uk')
-                    ->setTo('email@email.com')
+                    ->setTo('kadoshms@gmail.com')
                     ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
                 $this->get('mailer')->send($message);
-                $this->get('session')->setFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
-
-               // return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+                //$this->get('session')->setFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+                $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+               return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
             }else{
                 echo "Error";
             }
